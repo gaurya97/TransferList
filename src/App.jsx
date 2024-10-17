d52aac6398e2c3fe85cf4aa8adf6a32aaf07652d
gaurya97 <119463762+gaurya97@users.noreply.github.com>
2024-10-17 20:56:46 +0530

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
 import { useState } from "react";
 import "./styles.css";
+import { idText } from "typescript";
 const cell = (order) => {};
 
 export default function App() {
-  const data = ["first", "second", "third", "fourth"];
+  const data = [
+    { name: "first", cheaked: false, id: 1 },
+    { name: "second", cheaked: false, id: 2 },
+    { name: "third", cheaked: false, id: 3 },
+    { name: "fourth", cheaked: false, id: 4 },
+  ];
 
-  const [listLeft, setListLeft] = useState(data);
+  const [listLeft, setListLeft] = useState([...data]);
   const [listRight, setListRight] = useState([]);
-  const handleClick = (e, posistion) => {
-    console.log(e.target.innerText);
-    if (posistion == "left") {
-      let newLeftList = listLeft.filter((e) => e != e.target.innerText);
+
+  const toggleList = (list, id, cheaked) => {
+    return list.map((item) => {
+      if (item.id == id) {
+        return { ...item, cheaked: !cheaked };
+      } else {
+        return item;
+      }
+    });
+  };
+
+  const toggle = (list) => {
+    return list.map((item) => {
+      if (item.cheaked) {
+        return { ...item, cheaked: !item.cheaked };
+      } else {
+        return item;
+      }
+    });
+  };
+
+  const handleClick = (id, checked, position) => {
+    if (position == "left") {
+      let copydata = [...listLeft];
+      copydata = toggleList(copydata, id, checked);
+      setListLeft(copydata);
     } else {
-      let newRightList = listLeft.filter((e) => e != e.target.innerText);
+      let copydata = [...listRight];
+      copydata = toggleList(copydata, id, checked);
+      setListRight(copydata);
     }
   };
 
@@ -22,32 +52,55 @@ export default function App() {
         {listLeft.map((element, i) => {
           return (
             <div
-              key={element}
-              onClick={(e) => {
-                handleClick(e, "left");
-              }}
-              className="button"
+              key={element.name}
+              onClick={(e) => handleClick(element.id, element.cheaked, "left")}
+              className={element.cheaked ? "button border" : "button"}
             >
-              {element}
+              {element.name}
             </div>
           );
         })}
       </div>
       <div className="t-box">
-        <button>left</button>
-        <button>Right</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            let rightdata1 = listLeft.filter((el) => {
+              return el.cheaked == true;
+            });
+            rightdata1 = toggle(rightdata1);
+
+            let leftdata1 = listLeft.filter((el) => {
+              return el.cheaked == false;
+            });
+            leftdata1 = toggle(leftdata1);
+
+            let leftdata2 = listRight.filter((el) => {
+              return el.cheaked == true;
+            });
+            leftdata2 = toggle(leftdata2);
+
+            let rightdata2 = listRight.filter((el) => {
+              return el.cheaked == false;
+            });
+            rightdata2 = toggle(rightdata2);
+            setListLeft([...leftdata1, ...leftdata2]);
+            setListRight([...rightdata1, ...rightdata2]);
+          }}
+        >
+          Move
+        </button>
       </div>
       <div className="box">
         {listRight.map((element, i) => {
           return (
             <div
-              key={element}
-              onClick={(e) => {
-                handleClick(e, "right");
-              }}
-              className="button"
+              key={element.name}
+              onClick={(e) => handleClick(element.id, element.cheaked, "right")}
+              className={element.cheaked ? "button border" : "button"}
             >
-              {element}
+              {element.name}
             </div>
           );
         })}
